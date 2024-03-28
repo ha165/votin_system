bce17cbeb8db26b3fb56dd72a9fe6ccc4aef05c9
Harmony Lumumba Muyagane <lumumbaharmony@gmail.com>
2024-03-28 15:53:32 +0300

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -104,4 +104,5 @@ Route::delete('/elections/{elections}', [ElectionsController::class, 'destroy'])
 Route::get('/generate-pdf', [CandidatesController::class, 'generatePDF'])->name('generate-pdf');
 
 Route::get('/ballot', [BallotController::class, 'index'])->name('ballot.index');
-Route::get('/ballot/save', [BallotController::class, 'index'])->name('save_leader');
+
+Route::get('/ballot/save', [BallotController::class, 'create_votes'])->name('save_leader');
